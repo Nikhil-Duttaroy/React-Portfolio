3a157c668fcc8ce7f01e0cfadf9ba6918eb872e5
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2022-09-03 18:15:41 +0530

    base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import IntroPage from './Pages/IntroPage/IntroPage';
+import StackPage from './Pages/StackPage/StackPage';
+import ProjectsPage from './Pages/ProjectsPage/ProjectsPage';
+import ContactPage from './Pages/ContactPage/ContactPage';
 
 function App() {
   
 
   return (
     <div className="App">
-      
+      <IntroPage></IntroPage>
+      <StackPage></StackPage>
+      <ProjectsPage></ProjectsPage>
+      <ContactPage></ContactPage>
     </div>
   )
 }

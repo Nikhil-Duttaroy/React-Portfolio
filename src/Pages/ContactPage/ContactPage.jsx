2651d4878a1b731e3356570e17b02b8d5a3901fd
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2022-09-10 12:29:30 +0530

    netlify form added

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -11,36 +11,35 @@ const ContactPage = () => {
           id='contact-form'
           action='POST'
           name='ContactForm'
-          // data-netlify='true'
-          // data-netlify-recaptcha='true'
-          // netlify-honeypot='bot-field'
+          data-netlify='true'
+          data-netlify-recaptcha='true'
+          netlify-honeypot='bot-field'
         >
-          {/* <i class='fas fa-times fa-1x close'></i>
-          <h2 style='text-align: center'>Get in Touch</h2> */}
-
           <label>Name</label>
           <input
             required
-            class='input-field'
+            className='input-field'
             type='text'
             name='name'
             placeholder='Guest'
           />
 
           <label>Subject</label>
-          <input required class='input-field' type='text' name='subject' placeholder='I want a XYZ'/>
+          <input required className='input-field' type='text' name='subject' placeholder='I want a XYZ'/>
 
           <label>Email</label>
-          <input required class='input-field' type='text' name='email' placeholder='Guest@email.com'/>
+          <input required className='input-field' type='text' name='email' placeholder='Guest@email.com'/>
 
           <label>Message</label>
-          <textarea required class='input-field' name='message' placeholder='Your Message'></textarea>
-          {/* <p class='hidden' style='visibility: hidden; height: 0'>
+          <textarea required className='input-field' name='message' placeholder='Your Message'></textarea>
+
+          <p className='hidden' style={{visibility: 'hidden', height: '0'}}>
             <label>
               Don’t fill this out if you're human: <input name='bot-field' />
             </label>
           </p>
-          <div data-netlify-recaptcha='true'></div> */}
+
+          <div data-netlify-recaptcha='true'></div>
           <div className='buttonContainer'>
             <button id='submit-btn' type='submit' value='Send'>
               Submit Form

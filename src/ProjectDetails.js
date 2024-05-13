cf70a2d82c595095bf0434d4d944b2f26bf9a1b0
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2024-05-13 03:53:46 +0000

    ProjectDetails : Removed older Projects.

--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -20,33 +20,4 @@ export const ProjectDetails = [
     siteLink: "https://reactytclone.web.app/login",
     githubLink: "https://github.com/Nikhil-Duttaroy/Youtube-Clone",
   },
-  {
-    id: 4,
-    Name: "Quiz IT WebApp",
-    imgLink: "assets/images/QuizIT.png",
-    siteLink: "https://intpprojectquizit.000webhostapp.com/QuizIT/",
-    githubLink: "https://github.com/Nikhil-Duttaroy/QuizIT",
-  },
-  {
-    id: 5,
-    Name: "React Contact Manager",
-    imgLink: "assets/images/contact.png",
-    siteLink: "https://react-contactmanager.herokuapp.com/",
-    githubLink:
-      "https://github.com/Nikhil-Duttaroy/React-Materialize-Contact-Manager",
-  },
-  {
-    id: 6,
-    Name: "Basic Portfolio Design",
-    imgLink: "assets/images/basic.png",
-    siteLink: "https://simple-portfolio-design.netlify.app",
-    githubLink: "https://github.com/Nikhil-Duttaroy/Basic-Portfolio",
-  },
-  {
-    id: 7,
-    Name: "LivSpace",
-    imgLink: "assets/images/Livspace.png",
-    siteLink: "https://livspace-clone.netlify.app",
-    githubLink: "https://github.com/Nikhil-Duttaroy/Livspace",
-  },
 ];
